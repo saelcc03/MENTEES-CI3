a8391cabc4466ff53fd106009d67798b3fef66ae
Samuel E. Cano <137658174+saelcc03@users.noreply.github.com>
2023-06-24 20:01:45 -0500

    Create SamuelCano.cpp

---
+++ Complejidad algorítmica/SamuelCano.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define maxn 1000000000
+int32_t main(){
+    int n;
+    cin>>n;
+    int a[n];
+    vector<int>mxnow;
+    mxnow.asign
+    int  
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        
+    }
+}
